7a4330a14b6fd10d1f82644e45cdbc45784c0043
Juan Camilo Herrera <juan.c.herrerav@hotmail.com>
2016-04-25 11:58:10 -0500

    shopping kart con seguridad

--- src/main/java/edu/eci/pdsw/webappsintro/controller/ShoppingKartBackingBean.java
+++ src/main/java/edu/eci/pdsw/webappsintro/controller/ShoppingKartBackingBean.java
@@ -18,7 +18,11 @@ package edu.eci.pdsw.webappsintro.controller;
 
 import edu.eci.pdsw.stubs.servicesfacadestub.Producto;
 import edu.eci.pdsw.stubs.servicesfacadestub.ServicesFacade;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -26,12 +30,106 @@ import javax.faces.bean.SessionScoped;
  *
  * @author hcadavid
  */
+@SessionScoped
+@ManagedBean(name = "ShoppingKartBackingBean")
 public class ShoppingKartBackingBean {
-    
-    
+     private List<Producto> productos;
+     private Producto selectedProduct;
+     private List<Producto> selectedProducts = new ArrayList<Producto>();
+     private int cantidad;
+     private HashMap<Integer, Integer> cantidades = new HashMap<Integer,Integer>();
+     private int pagar = 0;
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad=cantidad;
+    }
+
+    public HashMap<Integer, Integer> getCantidades() {
+        return cantidades;
+    }
+
+    public void setCantidades(HashMap<Integer, Integer> cantidades) {
+        this.cantidades = cantidades;
+    }
+     
+    public List<Producto> getSelectedProducts() {
+        return selectedProducts;
+    }
+
+    public void setSelectedProducts(List<Producto> selectedProducts) {
+        this.selectedProducts = selectedProducts;
+    }
+
+    public Producto getSelectedProduct() {
+        
+        return selectedProduct;
+    }
+
+    public void setSelectedProduct(Producto selectedProduct) {
+        
+        this.selectedProduct = selectedProduct;
+        
+        
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
+     
     public List<Producto> getProductos(){
-        return ServicesFacade.getInstance().getProductos();
+        productos = ServicesFacade.getInstance().getProductos();
+        return productos;
     }
     
-    
+    public void add(){
+        if(this.cantidad>0){
+            if(!cantidades.containsKey(this.selectedProduct.getId())){
+                
+                this.selectedProducts.add(this.selectedProduct);
+                this.cantidades.put(this.selectedProduct.getId(), this.cantidad);
+            }
+            else{
+                
+                int temp = this.cantidades.get(this.selectedProduct.getId())+this.cantidad;
+                this.cantidades.replace(this.selectedProduct.getId(),temp);
+            }
+        }
+        else if(this.cantidad<0 && cantidades.containsKey(this.selectedProduct.getId())){
+            
+            if((this.cantidad+this.cantidades.get(this.selectedProduct.getId()))<=0){
+                this.selectedProducts.remove(this.selectedProduct);
+                this.cantidades.remove(this.selectedProduct.getId());
+            }
+            else if((this.cantidad+this.cantidades.get(this.selectedProduct.getId()))>0){
+                int temp = this.cantidades.get(this.selectedProduct.getId())+this.cantidad;
+                this.cantidades.replace(this.selectedProduct.getId(),temp);
+            }
+        }
+    }
+    public int getPagar() {
+        
+        return pagar;
+    }
+
+    public void setPagar(int pagar) {
+        this.pagar = pagar;
+    }
+    public void buy(){
+        int valor = 0;
+        
+        if (cantidades.isEmpty()){
+            valor = 0;
+        }
+        else{
+            for(int i=0; i<selectedProducts.size();i++){
+                Producto pd= selectedProducts.get(i);
+                valor+=(pd.getPrecioEnPesos()*cantidades.get(pd.getId()));
+            }
+        }
+        this.pagar=valor;
+    }
 }
